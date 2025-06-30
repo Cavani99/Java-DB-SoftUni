ca9fa747d5c87f5f20587a911c0386880ee0c249
Cavani99 <kristiian.donchev@abv.bg>
2025-06-30 21:26:07 +0300

    Made Task 8 in Introduction to Hibernate - Exercise

---
+++ Spring Data/Introduction to Hibernate/src/main/java/orm/GetEmployeesWithProject.java
@@ -0,0 +1,48 @@
+package orm;
+
+
+import entities.Employee;
+import entities.Project;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class GetEmployeesWithProject {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("soft_uni");
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Write employee id: ");
+        String employeeId = sc.nextLine();
+
+        Employee employee = em.createQuery(
+                        "SELECT e FROM Employee e " +
+                                "WHERE e.id = :id", Employee.class)
+                .setParameter("id", employeeId)
+                .getSingleResult();
+
+
+        System.out.println("Employee: " + employee.getFirstName());
+        List<Project> projects = employee.getProjects().stream()
+                .sorted(Comparator.comparing(Project::getName))
+                .collect(Collectors.toList());
+        StringBuilder projectText = new StringBuilder();
+        for (Project project : projects) {
+            projectText.append("   ").append(project.getName()).append("\n");
+        }
+
+        System.out.printf("%s %s - %s\n%s", employee.getFirstName(), employee.getLastName(), employee.getJobTitle(), projectText);
+
+        em.getTransaction().commit();
+        em.close();
+        factory.close();
+    }
+}
